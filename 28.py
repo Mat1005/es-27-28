6a2a4ae2ac9407128c256baef33ca18faee965b2
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-02-08 12:36:44 +0100

    Update 28.py

--- 28.py
+++ 28.py
@@ -3,9 +3,10 @@ I CAP vengono inseriti e memorizzati in un dizionario.
 Fornito poi il nome della città crea un programma che visualizzi il suo CAP e viceversa.
 '''
 CAP_dizionario = {}
-Città_dizionario = {}
+Citta_dizionario = {}
 tot = int(input())
-While True:
-    nome_città = ("Inserire il nome della città")
+for n in range (1, tot + 1):
+    nome_citta = ("Inserire il nome della città")
     CAP = int(input("Inserire il CAP"))
-    CAP_dizionario[CAP] = nome_città
+    CAP_dizionario[CAP] = nome_citta
+    Citta_dizionario[nome_citta] = CAP
