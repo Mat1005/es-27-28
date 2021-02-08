52e764439797f0a3921337d8e1631952ed9b932d
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-02-08 12:24:21 +0100

    Add files via upload

---
+++ 27.py
@@ -0,0 +1,17 @@
+'''
+Organizza con un dizionario la rubrica telefonica.
+Fornito poi il numero della persona visualizzare poi il numerto o inviare un messaggio in caso non sia prewszente nella lista.
+
+'''
+rubrica = {"Matteo Zaccarelli":"366 042 8604",
+"Simone Melli":"333 184 0037",
+"Enrico Rossi":"355 536 2242",
+"Riccardo Malavasi":"367 264 1131",
+"Giorgio Bondavalli":"354 242 4523",
+"Marco Antonio":"346 252 5563",
+"Luca Bianchi":"353 942 4324"}
+nome = input("Di chi vuoi sapere il numero?")
+if nome in rubrica:
+    print("Il numero del contatto ",nome," è:"rubrica[nome])
+else:
+    print("Il contatto ",nome," non è in rubrica.")
