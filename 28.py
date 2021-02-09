3088f243bba0b6fcfb80c9270c4504a3256e3363
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-02-09 17:01:50 +0100

    Update 28.py

--- 28.py
+++ 28.py
@@ -4,9 +4,20 @@ Fornito poi il nome della città crea un programma che visualizzi il suo CAP e v
 '''
 CAP_dizionario = {}
 Citta_dizionario = {}
-tot = int(input())
+tot = int(input("Quante sono le città?"))
 for n in range (1, tot + 1):
     nome_citta = ("Inserire il nome della città")
     CAP = int(input("Inserire il CAP"))
     CAP_dizionario[CAP] = nome_citta
     Citta_dizionario[nome_citta] = CAP
+citta = input("Di quale città vuoi sapere il CAP ?")
+if citta in Citta_dizionario:
+    print("Il Cap di ",citta," è:",Citta_dizionario[citta])
+else:
+    print("La città",citta," non è nella lista.")
+cap = input("Inserire il CAP di cui vuoi conoscere la città ")
+if cap in CAP_dizionario:
+    print("Il Cap ",cap," è della città di ",CAP_dizionario[cap])
+else:
+    print("Il cap",cap," non è nella lista.")
+   
